b54d530922f2d183498a51288fde34b463a65ba6
Yashah <yashah0399@gmail.com>
2019-04-01 08:42:33 +0530

    Initial commit

---
+++ app/src/main/java/android/tyit/com/androidresources/MainActivity.kt
@@ -0,0 +1,12 @@
+package android.tyit.com.androidresources
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
